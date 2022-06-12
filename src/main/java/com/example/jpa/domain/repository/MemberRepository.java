bbfee852ed529e77d9b6b11bc7f8027417bbcbe4
JJinJong <akwnsldj1@naver.com>
2022-06-12 18:41:41 +0900

    Spring Data Jpa

---
+++ src/main/java/com/example/jpa/domain/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.jpa.domain.repository;
+
+import com.example.jpa.domain.vo.MemberVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberVO, Long> {
+
+}
---
+++ src/main/java/com/example/jpa/domain/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.example.jpa.domain.repository;
+
+import com.example.jpa.domain.vo.OrderVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderVO, Long> {
+//    void deleteByProductNumber(Long productNumber);
+}
